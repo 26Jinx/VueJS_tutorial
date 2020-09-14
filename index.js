2ad97c6f71f593a0e8ef980c81cd0e3b72e42a0c
PharahMain <26randomforests@gmail.com>
2020-09-14 12:51:16 -0700

    user input practice

--- index.js
+++ index.js
@@ -11,3 +11,40 @@ var app2 = new Vue({
         message: 'You loaded this page on ' + new Date().toLocaleDateString()
     }
 })
+
+var app3 = new Vue({
+    el: '#app-3',
+    data: {
+        seen: true
+    }
+})
+
+var app4 = new Vue({
+    el: '#app-4',
+    data: {
+        todos: [
+            {text: 'Learn JavaScript'},
+            {text: 'Learn Vue'},
+            {text: 'Build something awesome'}
+        ]
+    }
+})
+
+var app5 = new Vue({
+    el: '#app-5',
+    data: {
+        message: 'hello vue.js'
+    },
+    methods: {
+        reverseMessage: function () {
+            this.message = this.message.split('').reverse().join('')
+        }
+    }
+})
+
+var app6 = new Vue({
+    el: '#app-6',
+    data: {
+        message: 'Type in the box to see this change!'
+    }
+})
